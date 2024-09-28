b72e1c643d51c5e7b2e83a9c46dcb52b2586b79d
Dharani4432 <119032451+Dharani4432@users.noreply.github.com>
2024-09-29 00:03:48 +1000

    initial cmmit

---
+++ inventory_management/app/config.py
@@ -0,0 +1,3 @@
+import os
+
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://dharani722w8_user:cfIrj4OSU2CtbGJ6DfkCTTLBugxr855h@dpg-cronrlhu0jms73ccha60-a.oregon-postgres.render.com/dharani722w8')
---
+++ scripts/terraform/resource-group.tf
@@ -0,0 +1,5 @@
+
+resource "azurerm_resource_group" "dharanitask8" {
+  name     = var.app_name
+  location = var.location
+}
